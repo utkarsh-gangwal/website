8bad547789df123cbc6a4b750fcf1e1e58959a30
Utkarsh Gangwal <73367125+utkarsh-gangwal@users.noreply.github.com>
2025-06-08 12:32:06 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
+    // Sticky navbar on scroll
     $(window).scroll(function(){
-        // sticky navbar on scroll script
         if(this.scrollY > 10){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-        
-        // scroll-up button show/hide script
+
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -15,21 +14,18 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 });
+
